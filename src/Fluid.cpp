2d08fe3302d7b3c7200ad8ac3c10497a5cf91784
Sungki <youbyme@gmail.com>
2021-04-02 10:55:59 -0400

    Added Diffuse function

--- src/Fluid.cpp
+++ src/Fluid.cpp
@@ -2,6 +2,8 @@
 
 Fluid::Fluid(int N, float _dt, float diffusion, float viscosity)
 {
+    iter = 10;
+
     size = N;
     dt = _dt;
     diff = diffusion;
@@ -27,4 +29,47 @@ Fluid::~Fluid()
 
     delete[] density;
     delete[] s;
+}
+
+int Fluid::IX(int x, int y)
+{
+    return x + y * size;
+}
+
+void Fluid::AddDensity(int x, int y, float amount)
+{
+    int index = IX(x, y);
+    density[index] += amount;
+}
+
+void Fluid::AddVelocity(int x, int y, float amountX, float amountY)
+{
+    int index = IX(x, y);
+    Vx[index] += amountX;
+    Vy[index] += amountY;
+}
+
+void Fluid::Diffuse(int b, float* x, float* x0, float diff, float dt)
+{
+    float a = dt * diff * (size - 2) * (size - 2);
+    LinSolve(b, x, x0, a, 1 + 6 * a);
+}
+
+void Fluid::LinSolve(int b, float* x, float* x0, float a, float c)
+{
+    float cRecip = 1.0 / c;
+    for (int k = 0; k < iter; k++) {
+        for (int j = 1; j < size - 1; j++) {
+            for (int i = 1; i < size - 1; i++) {
+                x[IX(i, j)] =
+                    (x0[IX(i, j)]
+                        + a * (x[IX(i + 1, j)]
+                            + x[IX(i - 1, j)]
+                            + x[IX(i, j + 1)]
+                            + x[IX(i, j - 1)]
+                            )) * cRecip;
+            }
+        }
+//        set_bnd(b, x, N);
+    }
 }
--- src/Fluid.h
+++ src/Fluid.h
@@ -1,10 +1,13 @@
 #pragma once
+
 class Fluid
 {
     int size;
     float dt;
     float diff;
     float visc;
+
+    int iter;
 
     float* s;
     float* density;
@@ -14,6 +17,13 @@ class Fluid
 
     float* Vx0;
     float* Vy0;
+
+    int IX(int x, int y);
+    void AddDensity(int x, int y, float amount);
+    void AddVelocity(int x, int y, float amountX, float amountY);
+
+    void Diffuse(int b, float* x, float* x0, float diff, float dt);
+    void LinSolve(int b, float* x, float* x0, float a, float c);
 
 public:
     Fluid(int N, float _dt, float diffusion, float viscosity);
